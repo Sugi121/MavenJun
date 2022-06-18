65c7c7458fe188e44caa1bdd614621db20504245
Sugi121 <suganyaperumal01@gmail.com>
2022-06-18 14:45:33 +0530

    Login functionality

---
+++ src/com/data/LoginPage.java
@@ -0,0 +1,21 @@
+package com.data;
+
+public class LoginPage {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("User in Login Page");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
